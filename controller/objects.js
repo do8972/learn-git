d1aeeb02aae6d6d183ffd832c295c9ac54db569e
do8972 <do8972@gmail.com>
2021-12-19 20:13:57 +0900

    Fix: getObject

--- controller/objects.js
+++ controller/objects.js
@@ -11,7 +11,7 @@ module.exports = {
       // const data = "blob 12\0Hello, Git!\n";
       const hashed = crypto.createHash("sha1").update(data).digest("hex");
       const dirName = [hashed.substring(0, 2), hashed.substring(2)];
-      const compression = deflateSync(hashed, { level: 1 }); // level1로 압축 (내용으로 들어감.)
+      const compression = deflateSync(data, { level: 1 }); // level1로 압축 (내용으로 들어감.)
       console.log(hashed, compression);
 
       fs.existsSync(`../.my-git/${dirName[0]}`);
@@ -25,23 +25,29 @@ module.exports = {
   },
   getObject: async (req, res) => {
     try {
-      // dir에 있는 파일을 확인한다.
-      // 찾은 파일을 압축푼다. => sha1 해시값이 나옴.
-      // 해시란 단방향 암호화라 풀 수가 없음..
-      // git cat-file (-p | -t | -s)는 해시를 풀어야만 알 수 있음..
-
       const regex = new RegExp(`[a-zA-Z0-9]{40}`);
+      const regexNum = /[^0-9]/g;
+      const regexContent = /(?<=\0).*[^\n]/g;
+
       const objectId = req.params.objectId;
       const dirName = [objectId.substring(0, 2), objectId.substring(2)];
 
       if (!regex.exec(objectId))
         return res.status(400).json("ClientError: objectId");
-      // const files = await readdir(`../.my-git/${dirName[0]}`);
 
       const find = fs.readFileSync(`../.my-git/${dirName[0]}/${dirName[1]}`);
       const unzip = zlib.inflateSync(find);
 
-      return res.status(200).json(unzip.toString());
+      const content = unzip.toString().split(" ");
+      const mainConent = content.slice(1).join(" ");
+
+      return res.status(200).json({
+        data: {
+          type: content[0],
+          content: regexContent.exec(mainConent)[0],
+          size: content[1].replace(regexNum, ""),
+        },
+      });
     } catch (error) {
       console.log(error);
       return res.status(404).json("NotFoundError: objectId");
