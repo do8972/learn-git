b46f3168a08b0d4f5b41dcf2aa9d3f15ba51a3a0
do8972 <do8972@gmail.com>
2021-12-19 20:39:01 +0900

    Fix: putObjects

--- controller/objects.js
+++ controller/objects.js
@@ -12,9 +12,7 @@ module.exports = {
       const hashed = crypto.createHash("sha1").update(data).digest("hex");
       const dirName = [hashed.substring(0, 2), hashed.substring(2)];
       const compression = deflateSync(data, { level: 1 }); // level1로 압축 (내용으로 들어감.)
-      console.log(hashed, compression);
 
-      fs.existsSync(`../.my-git/${dirName[0]}`);
       fs.mkdirSync(`../.my-git/${dirName[0]}`, { recursive: true });
       fs.writeFileSync(`../.my-git/${dirName[0]}/${dirName[1]}`, compression);
       return res.status(200).json({ objectId: hashed });
