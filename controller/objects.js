4178ee7301fc8c1e2ffb5798195298b72d5d0aed
do8972 <do8972@gmail.com>
2021-12-27 13:20:43 +0900

    Fix: objects.js

--- controller/objects.js
+++ controller/objects.js
@@ -3,24 +3,27 @@ const zlib = require("zlib");
 const fs = require("fs");
 const crypto = require("crypto");
 
+const mygitDirPath = "./.my-git"
+const objectDirPath = `${mygitDirPath}/objects`
+
 module.exports = {
   putObject: async (req, res) => {
     const { content, objectType, write } = req.body;
     const data = `${objectType} ${content.length}\0${content}`;
-    
+
     try {
       const hashed = crypto.createHash("sha1").update(data).digest("hex");
       const dirName = [hashed.substring(0, 2), hashed.substring(2)];
       const compression = deflateSync(data, { level: 1 }); // level1로 압축 (내용으로 들어감)
 
-      if (write && !fs.existsSync(`../.my-git/${dirName[0]}`)) {
-        fs.mkdirSync(`../.my-git/${dirName[0]}`, { recursive: true });
-        fs.writeFileSync(`../.my-git/${dirName[0]}/${dirName[1]}`, compression);
+      if (write && !fs.existsSync(`${objectDirPath}/${dirName[0]}`)) {
+        fs.mkdirSync(`${objectDirPath}/${dirName[0]}`, { recursive: true });
+        fs.writeFileSync(`${objectDirPath}/${dirName[0]}/${dirName[1]}`, compression);
       }
 
       return res.status(200).json({ objectId: hashed });
     } catch (error) {
-      console.log(error);
+      console.error(error);
       return res.sendStatus(500);
     }
   },
@@ -28,36 +31,38 @@ module.exports = {
     const objectId = req.params.objectId;
 
     try {
-      const ckeckRegex = new RegExp(`[a-zA-Z0-9]{4,40}`);
+      const ckeckRegex = new RegExp("[a-zA-Z0-9]{4,40}");
       const objectPattern =
-        /^(?<type>blob|commit|tree|tag) (?<size>\d+)\0(?<content>.*)\n$/;
+        /^(?<type>blob|commit|tree|tag) (?<size>\d+)\0(?<content>(.|\s)*)$/;
 
       if (!ckeckRegex.exec(objectId))
         return res.status(400).json(`ClientError: ${objectId}`);
       
       const dirName = [objectId.substring(0, 2), objectId.substring(2)];
       
-      if (!fs.existsSync(`../.my-git/${dirName[0]}`)) {
+      if (!fs.existsSync(`${objectDirPath}/${dirName[0]}`)) {
         return res.status(404).json(`NotFoundError: ${objectId}`);
       }
 
-      const fileNameChack = fs.readdirSync(`../.my-git/${dirName[0]}`)
-      .filter((fileName) => {
-        return fileName.slice(0, dirName[1].length) === dirName[1]})
+      const filesMatched = fs.readdirSync(`${objectDirPath}/${dirName[0]}`)
+        .filter(fileName => fileName.startsWith(dirName[1]));
+      
 
-      if (fileNameChack.length >= 2) {
-        return res.status(400).json(`overlappingFiles: ${fileNameChack}`)
+      if (filesMatched.length === 0) {
+        return res.status(404).json(`NotFoundError: ${objectId}`);
+      } else if (filesMatched.length > 1) {
+        return res.status(400).json(`overlappingFiles: ${filesMatched}`);
       }
       
-      const find = fs.readFileSync(`../.my-git/${dirName[0]}/${fileNameChack}`);
+      const find = fs.readFileSync(`${objectDirPath}/${dirName[0]}/${filesMatched[0]}`);
       const unzip = zlib.inflateSync(find);
 
       return res.status(200).json({
         data: { ...objectPattern.exec(unzip.toString()).groups },
       });
     } catch (error) {
-      console.log(error);
-      return res.status(404).json(`NotFoundError: ${objectId}`);
+      console.error(error)
+      return res.status(500).json('Unknwon Server error');
     }
   },
 };
